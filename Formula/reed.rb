afb471b710da6dfb3bd6ffbfc0d6d16f93da0999
Azis <azuolas.krusna@yahoo.com>
2026-02-16 19:05:05 +0100

    Try install deps

--- Formula/reed.rb
+++ Formula/reed.rb
@@ -1,6 +1,4 @@
 class Reed < Formula
-  include Language::Python::Virtualenv
-
   desc "A CLI that reads text aloud using piper-tts"
   homepage "https://github.com/AzisK/Reed"
   url "https://files.pythonhosted.org/packages/source/r/reedy/reedy-0.1.0.tar.gz"
@@ -10,9 +8,10 @@ class Reed < Formula
   depends_on "python@3.14"
 
   def install
-    venv = virtualenv_create(libexec, "python3.14")
-    system libexec/"bin/pip", "install", "reedy==#{version}"
-    bin.install_symlink Dir[libexec/"bin/reed"]
+    python = Formula["python@3.14"].opt_bin/"python3.14"
+    system python, "-m", "venv", libexec
+    system libexec/"bin/python3", "-m", "pip", "install", "reedy==#{version}"
+    bin.install_symlink libexec/"bin/reed"
   end
 
   test do
